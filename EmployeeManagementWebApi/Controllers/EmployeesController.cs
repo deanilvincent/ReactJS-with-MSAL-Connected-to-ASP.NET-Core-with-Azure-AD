15eca7befb1dac2991b802b0c2851d39bab17d6c
Mark Vicente <mark@anynetworksystems.com>
2022-06-30 23:14:45 +0800

    Initial Commit

---
+++ EmployeeManagementWebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace EmployeeManagementWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:scopes")]
+    public class EmployeesController : ControllerBase
+    {
+        private static readonly string[] Employees = new[] { "Anna Williams", "Nina Williams", "Paul Phoenix", "Eddy Gordo" };
+        [Authorize(Roles = "Admin")]
+        public IActionResult GetEmployees()
+        {
+            return Ok(Employees);
+        }
+
+        [Authorize]
+        [HttpGet("total-employees")]
+        public IActionResult TotalEmployees()
+        {
+            return Ok(Employees.Length);
+        }
+    }
+}
---
+++ EmployeeManagementWebApi/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration);
+
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy", policy =>
+{
+    policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
+}));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
